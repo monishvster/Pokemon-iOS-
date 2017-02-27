739f677c3a92e235ffec2d929f7e7b20e7249869
Verma <s528180@ol23204m.nwmissouri.edu>
2017-02-26 19:30:19 -0600

    Final Commit

--- Pokemon/PokemonFactory.swift
+++ Pokemon/PokemonFactory.swift
@@ -2,17 +2,8 @@
 //  PokemonFactory.swift
 //  Pokemon
 //
-//  Created by Verma,Monish on 2/25/17.
+//  Created by Verma,Monish on 2/26/17.
 //  Copyright © 2017 Verma,Monish. All rights reserved.
 //
 
 import Foundation
-import UIKit
-class PokemonFactory {
-func createModel() {
-    var pokemon:Pokemon
-    
-    
-}
-
-}
